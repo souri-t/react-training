cd0e0082f6d7eff22851b14f5b6fba1197d6ef16
yosuke <>
2023-07-01 22:14:33 +0900

    fix

--- src/Pages/public/BigCalendarPage.tsx
+++ src/Pages/public/BigCalendarPage.tsx
@@ -1,48 +1,49 @@
-import React, { Component } from 'react';
-import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer, SlotInfo } from 'react-big-calendar';
 import moment from 'moment';
 
-const localizer = momentLocalizer(moment);
-const eventList = [
-  {
-    id: 0,
-    title: 'All Day Event very long title',
-    allDay: true,
-    start: new Date('2023-06-01'),
-    end: new Date('2023-06-01'),
-  },
-  {
-    id: 1,
-    title: 'Long Event',
-    allDay: false,
-    start: new Date('2023-06-25T15:00:00'),
-    end: new Date('2023-06-25T17:00:00'),
-  },
-  {
-    id: 2,
-    title: 'Event',
-    allDay: false,
-    start: new Date('2023-07-01T02:00:00'),
-    end: new Date('2023-07-01T04:00:00'),
-  }
-];
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-const BigCalendarPage = () => {
-  
+interface Event {
+  title: string;
+  start: Date;
+  end: Date;
+}
+
+const MyCalendar: React.FC = () => {
+  const [events, setEvents] = useState<Event[]>([
+    {
+      title: '予定1',
+      start: new Date('2023-07-01T02:00:00'),
+      end: new Date('2023-07-01T04:00:00'),
+    },
+    // 他の予定もここに追加する
+  ]);
+
+  const handleSelectSlot = (slotInfo: SlotInfo) => {
+    const { start, end } = slotInfo;
+    const newEvent: Event = {
+      title: '新しい予定',
+      start,
+      end,
+    };
+
+    setEvents((prevEvents) => [...prevEvents, newEvent]);
+  };
+
+  const localizer = momentLocalizer(moment);
+
   return (
     <div>
       <Calendar
         localizer={localizer}
-        events={eventList}
-        views={['week','day','month', 'agenda', 'work_week']} //未指定の場合は全て表示される
-        timeslots={2}
-        defaultView={Views.WEEK}
-        onSelectEvent={event => alert(event.title)}
-        style={{ height: 500 }} />
+        selectable={true}
+        onSelectSlot={handleSelectSlot}
+        events={events}
+        defaultView="week"
+      />
     </div>
   );
- 
-}
+};
 
-export default BigCalendarPage;
+export default MyCalendar;
---
+++ src/Pages/public/BigCalenderTestPage.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+
+const localizer = momentLocalizer(moment);
+const eventList = [
+  {
+    id: 0,
+    title: 'All Day Event very long title',
+    allDay: true,
+    start: new Date('2023-06-01'),
+    end: new Date('2023-06-01'),
+  },
+  {
+    id: 1,
+    title: 'Long Event',
+    allDay: false,
+    start: new Date('2023-06-25T15:00:00'),
+    end: new Date('2023-06-25T17:00:00'),
+  },
+  {
+    id: 2,
+    title: 'Event',
+    allDay: false,
+    start: new Date('2023-07-01T02:00:00'),
+    end: new Date('2023-07-01T04:00:00'),
+  }
+];
+
+const BigCalendarTestPage = () => {
+  
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={eventList}
+        views={['week','day','month', 'agenda', 'work_week']} //未指定の場合は全て表示される
+        timeslots={2}
+        defaultView={Views.WEEK}
+        onSelectEvent={event => alert(event.title)}
+        style={{ height: 500 }} />
+    </div>
+  );
+ 
+}
+
+export default BigCalendarTestPage;
