1a1707e51b6e0ecb4e1d6aececd4d50947e0d41d
yosuke <>
2023-07-02 04:00:42 +0900

    fix

--- src/Pages/public/BigCalendarPage.tsx
+++ src/Pages/public/BigCalendarPage.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer, SlotInfo } from 'react-big-calendar';
 import moment from 'moment';
+import axios from 'axios';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+//イベントの型定義
 interface Event {
   title: string;
   start: Date;
@@ -11,15 +13,31 @@ interface Event {
 }
 
 const MyCalendar: React.FC = () => {
-  const [events, setEvents] = useState<Event[]>([
-    {
-      title: '予定1',
-      start: new Date('2023-07-01T02:00:00'),
-      end: new Date('2023-07-01T04:00:00'),
-    },
-    // 他の予定もここに追加する
-  ]);
+  const [events, setEvents] = useState<Event[]>([]);
 
+  //初期表示時にデータを取得する
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/rest/schedules');
+        const data = response.data.schedules.map((schedule: any) => ({
+          title: schedule.title,
+          start: new Date(schedule.starttime),
+          end: new Date(schedule.endtime),
+        }));
+        console.log(data);
+
+        //取得したデータをセットする
+        setEvents(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  //スケジュールを追加する
   const handleSelectSlot = (slotInfo: SlotInfo) => {
     const { start, end } = slotInfo;
     const newEvent: Event = {
