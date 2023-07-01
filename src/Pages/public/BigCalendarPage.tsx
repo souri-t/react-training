611b92a2e2aef24880a08d820f955ab84c934b09
yosuke <>
2023-07-02 04:00:57 +0900

    fix

--- src/Pages/public/BigCalendarPage.tsx
+++ src/Pages/public/BigCalendarPage.tsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer, SlotInfo } from 'react-big-calendar';
 import moment from 'moment';
 import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-//イベントの型定義
 interface Event {
   title: string;
   start: Date;
@@ -14,8 +15,13 @@ interface Event {
 
 const MyCalendar: React.FC = () => {
   const [events, setEvents] = useState<Event[]>([]);
+  const [showModal, setShowModal] = useState(false);
+  const [newEvent, setNewEvent] = useState<Event>({
+    title: '',
+    start: new Date(),
+    end: new Date(),
+  });
 
-  //初期表示時にデータを取得する
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -25,9 +31,6 @@ const MyCalendar: React.FC = () => {
           start: new Date(schedule.starttime),
           end: new Date(schedule.endtime),
         }));
-        console.log(data);
-
-        //取得したデータをセットする
         setEvents(data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -37,16 +40,49 @@ const MyCalendar: React.FC = () => {
     fetchData();
   }, []);
 
-  //スケジュールを追加する
   const handleSelectSlot = (slotInfo: SlotInfo) => {
     const { start, end } = slotInfo;
-    const newEvent: Event = {
-      title: '新しい予定',
+    setNewEvent({
+      title: '',
       start,
       end,
-    };
+    });
+    setShowModal(true);
+  };
+
+  const handleModalSave = () => {
+    if (newEvent.title.trim() === '') {
+      alert('タイトルを入力してください');
+      return;
+    }
 
     setEvents((prevEvents) => [...prevEvents, newEvent]);
+    setShowModal(false);
+  };
+
+  const handleModalCancel = () => {
+    setShowModal(false);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewEvent((prevEvent) => ({
+      ...prevEvent,
+      title: event.target.value,
+    }));
+  };
+
+  const handleStartDateChange = (date: Date) => {
+    setNewEvent((prevEvent) => ({
+      ...prevEvent,
+      start: date,
+    }));
+  };
+
+  const handleEndDateChange = (date: Date) => {
+    setNewEvent((prevEvent) => ({
+      ...prevEvent,
+      end: date,
+    }));
   };
 
   const localizer = momentLocalizer(moment);
@@ -60,6 +96,38 @@ const MyCalendar: React.FC = () => {
         events={events}
         defaultView="week"
       />
+
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>新しい予定</h2>
+            <input
+              type="text"
+              value={newEvent.title}
+              onChange={handleTitleChange}
+              placeholder="タイトル"
+            />
+            <p>開始日時:</p>
+            <DatePicker
+                selected={newEvent.start}
+                onChange={handleStartDateChange}
+                showTimeSelect
+                dateFormat="yyyy-MM-dd HH:mm"
+              />
+            <p>終了日時:</p>
+            <DatePicker
+              selected={newEvent.end}
+              onChange={handleEndDateChange}
+              showTimeSelect
+              dateFormat="yyyy-MM-dd HH:mm"
+            />
+            <div className="modal-buttons">
+              <button onClick={handleModalSave}>保存</button>
+              <button onClick={handleModalCancel}>キャンセル</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
